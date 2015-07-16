57fc22268973dfc527a3dcc3d3ad343944d79bf8
Eric De Witte <eric.dewitte@emc.com>
2015-07-16 15:02:53 +0200

    fixing bugs

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 import os
 import uuid
+import urlparse
+import redis
+import json
+
+rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+credentials = rediscloud_service['credentials']
+r_server = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
+
 from flask import Flask
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
@@ -8,14 +16,19 @@ GREEN = "#33CC33"
 
 COLOR = BLUE
 
+#if r_server.exists('counter') is False:
+r_server.set('counter',1)
+
+
 @app.route('/')
 def hello():
-
+	global r_server
+	r_server.incr('counter')
 	return """
 	<html>
 	<body bgcolor="{}">
 
-	<center><h1><font color="white">Welcome to LCM App !<br/>
+	<center><h1><font color="white">Welcome to Redis App !<br/>
 	</center>
 
 
@@ -23,9 +36,15 @@ def hello():
 	{}
 	</center>
 
+	<center><h1><font color="white">Hits count:<br/>
+	{}
+	</center>
+
+
+
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
+	""".format(COLOR,my_uuid,r_server.get('counter'))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
