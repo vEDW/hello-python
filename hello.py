9b88a3af0daa0045748969fea5f023e6fa0fd2c0
Eric De Witte <eric.dewitte@emc.com>
2015-07-17 12:19:18 +0200

    debug required to spil ip addresses

--- hello.py
+++ hello.py
@@ -3,15 +3,18 @@ import uuid
 import urlparse
 import redis
 import json
+import urllib
+import requests
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
 
 rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
 credentials = rediscloud_service['credentials']
 r_server = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
 
-from flask import Flask
-
-from flask import request
-from flask import jsonify
 
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
@@ -20,16 +23,35 @@ GREEN = "#33CC33"
 
 COLOR = BLUE
 
+@app.route('/admin')
+
+def otherfct():
+	print "admin"
+
+
 @app.route('/')
+
 def hello():
 	global r_server
 	r_server.incr('counter')
 	if request.headers.getlist("X-Forwarded-For"):
    		ipaddress = request.headers.getlist("X-Forwarded-For")[0]
+		print "X-Forwarded-For : " + ipaddress
+		print type(ipaddress)
+#		newip = %s (ipaddress)
+#		print type(newip)
+#		print "newip : " + newip
+
 	else:
    		ipaddress = request.remote_addr
 
+#	response = urllib.request.urlopen('http://freegeoip.net/json/'+ipaddress[0]).read().decode('utf-8')
+	responseJson = requests.get('http://freegeoip.net/json/'+'213.49.119.86').json()
+	country = responseJson.get("country_code")
+#	country="test"
+
 	return """
+
 	<html>
 	<body bgcolor="{}">
 
@@ -48,9 +70,16 @@ def hello():
 	{}
 	</center>
 
+	<center><h1><font color="white">Your Country :<br/>
+	{}
+	</center>
+
 	</body>
 	</html>
-	""".format(COLOR,my_uuid,r_server.get('counter'),ipaddress)
+	""".format(COLOR,my_uuid,r_server.get('counter'),ipaddress,country)
+
+
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
+	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
