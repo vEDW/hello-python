5658bedf2d3f9625ec0d9168c49be7117f91c555
Eric De Witte <eric.dewitte@emc.com>
2016-01-21 15:16:10 +0100

    Andreas

--- hello.py
+++ hello.py
@@ -119,7 +119,7 @@ def hello():
 	<html>
 	<body bgcolor="{}">
 
-	<center><h1><font color="white">Welcome to Exxos !<br/>
+	<center><h1><font color="white">Welcome to Andreas !<br/>
 	</center>
 
 	<center><h1><font color="white">I'm GUID:<br/>
