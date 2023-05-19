d30fc29570929f402e310fff4d550a6b68005d54
Eric De Witte <vEDW@users.noreply.github.com>
2023-05-19 17:01:43 +0200

    test response
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- load-test.sh
+++ load-test.sh
@@ -4,8 +4,29 @@ URL="http://<ip or url>"
 
 declare -A answers
 
+get_result() {
+        #returns json
+        RESPONSE=$(curl -s -w '####%{response_code}' ${URL})
+        HTTPSTATUS=$(echo ${RESPONSE} |awk -F '####' '{print $2}')
+        case $HTTPSTATUS in
+
+                200)    
+                        echo ${RESPONSE} |awk -F '####' '{print $1}'
+                        ;;
+
+                503)    
+                        echo "Not Ready"
+                        ;;
+                *)      
+                         echo ${RESPONSE} |awk -F '####' '{print $1}'
+                        ;;
+
+        esac
+}
+
+
 while true; do
-    testresult=$(curl -s ${URL} )
+    testresult=$(get_result)
 
     if [[ ${answers[$testresult]} == "" ]]; then
         answers[$testresult]=1
